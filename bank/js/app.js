e74de936cb777b8c14dd3316f71c2c25c70bc75b
allone00 <raldugin06@mail.ru>
2020-08-02 19:31:49 +0400

    bank

---
+++ bank/js/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function RegisterButton(props) {
+    return <button>Hi</button>;
+}
+
+ReactDOM.render(<RegisterButton/>, document.querySelector(
+    '.main'
+));
+
+
---
+++ bank/webpack.config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+
+module.exports = {
+    mode: 'development',
+
+    entry: './js/app.js',
+
+    optimization: {
+        minimize: false
+    },
+
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        'presets': ['env', 'es2015', 'stage-0', 'stage-1', 'stage-2', 'react'],
+                        'plugins': ['transform-es2015-destructuring', 'transform-object-rest-spread']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+
+        ]
+    }
+};
