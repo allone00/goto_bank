a36fde1b6bdc710417d6adae1b3d58566f6ec7b2
Gepeusea <ulfinka@gmail.com>
2020-08-03 17:30:45 +0300

    'next_try_with_post'

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, jsonify, request
+import requests
 import os
 import logging
 app = Flask(__name__, static_folder='static')
 
 
-@app.route('/')
-def hello_world():
-    return render_template('auth.html')
+# @app.route('/')
+# def hello_world():
+#     return render_template('auth.html')
+
+@app.route('/', methods=['GET','POST'])
+def request_auth():
+    value = request.args.get('code')
+    url = 'https://stonks.goto.msk.ru/o/token/'
+    myobj = {'client_id': 'M2mY5d4b6NcVKxr2XqKXSxZgpk78WK6ZaU3IxYDd',
+             'client_secret': '81ASvQUU6xYJzGei9r1HwkIcR3xZgFHrkMBlHtl5FifykxbbodQNPRixEQ5RwN4K5MSJIdqn4xXLWxqKDzfQ5kxPzZoWde6OsZ1rmaz8TKMNA8aOMHQ4Raxj8okxo2bs',
+             'grant_type': 'authorization_code',
+             'code': value}
+
+
+    x = requests.post(url, data=myobj)
+    return x.text
+
 
 
 if __name__ == "__main__":
