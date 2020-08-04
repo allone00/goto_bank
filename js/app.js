be666a9f6b26163d7cd95a01c7897ea0694513be
allone00 <raldugin06@mail.ru>
2020-08-04 17:02:57 +0400

    asdf

--- js/app.js
+++ js/app.js
@@ -1,62 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import '../css/auth.css';
 import '../css/basic.css';
+import '../css/login.css';
 
-function AuthPage(props) {
-    return (
-        <div>
-            <Main/>
-            <Footer/>
-        </div>
-    )
-}
-
-function Main(props) {
-    return <main className={'main'}><Inner_container/></main>;
-}
-
-function Inner_container(props) {
-    return <div className={'inner_container'}>
-        <ContainerTitle/>
-        <ContainerText/>
-        <ButtonBox/>
-    </div>;
-}
-
-function ContainerTitle(props) {
-    return <h1 className={'container_title'}>GoToCredit</h1>
-}
-
-function ContainerText(props) {
-    return <h2 className={'container_text'}>Всего 3 шага до вашей мечты!</h2>
-}
-
-function Footer(props) {
-    return <footer className={'footer'}><Footer_text/></footer>;
-}
-
-function Footer_text(props) {
-    return <h3 className={'footer_text'}>По всем вопросам писать сюда - @tvorogme</h3>;
-}
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+} from "react-router-dom";
+import {FirstPage} from "./first_page";
 
 
-function LoginButton(props) {
-    return <a className={'LoginButton'} value={'login'} href={"#main_page"}>Login</a>;
-}
 
-function RegisterButton(props) {
-    return <a className={'RegisterButton'} value={'register'}  href={"https://stonks.goto.msk.ru/o/authorize/?state=random_state_string&client_id=M2mY5d4b6NcVKxr2XqKXSxZgpk78WK6ZaU3IxYDd&response_type=code"}>
-        Register
-    </a>;
-}
-
-function ButtonBox(props) {
-    return <div className={'ButtonBox'}>
-        <RegisterButton/>
-        <LoginButton/>
-    </div>;
-}
-
-ReactDOM.render(<AuthPage/>, document.getElementById('cool'));
+ReactDOM.render(<FirstPage/>, document.getElementById('cool'));
 
