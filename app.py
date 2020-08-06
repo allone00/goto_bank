95a18a8a32b4bd1e80356bece11a605ab5de168f
unknown <ilyamikheev2605@gmail.com>
2020-08-06 13:17:10 +0300

    fix this

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import json
 import pika
 app = Flask(__name__, static_folder='static')
 
+testing = True
+
 message_from_ui = {}
 credentials = pika.PlainCredentials("rabbitmq", "rabbitmq")
 parameters = pika.ConnectionParameters("rmq", 5672, "/", credentials)
@@ -19,42 +21,35 @@ channel.queue_declare(queue='api')
 def hello_world(path='/'):
     return render_template('auth.html')
 
-@app.route('/api/qwerty', methods=['GET'])
+@app.route('/api/qwerty', methods=['GET','POST'])
 def get_message():
-    app.logger.info("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa11111")
-    message_from_ui = request.json()
+    global testing
+    app.logger.info("debug 1")
+    message_from_ui = request.get_json()
     value = request.args.get('code')
+
     url = 'https://stonks.goto.msk.ru/o/token/'
-    myobj = {'client_id': 'M2mY5d4b6NcVKxr2XqKXSxZgpk78WK6ZaU3IxYDd',
-            'client_secret': '81ASvQUU6xYJzGei9r1HwkIcR3xZgFHrkMBlHtl5FifykxbbodQNPRixEQ5RwN4K5MSJIdqn4xXLWxqKDzfQ5kxPzZoWde6OsZ1rmaz8TKMNA8aOMHQ4Raxj8okxo2bs',
+    myobj = {'client_id': 'rvAyzLvmASDEw1DCDMShbdjNpsPR6I89IRiGgpoL', #if testing else 'M2mY5d4b6NcVKxr2XqKXSxZgpk78WK6ZaU3IxYDd',
+            'client_secret': 'nIUUEC0sQSvehn03I4UTIHcEZmT7yCjn5uUzwD02PqbikQNid5jFRWlulGXBt1gQsmlEWAgirCpsUwsOmMQFx3bfgq0nxuvDZEl31lM1sSDvRLrpevcTsh9NNxe5xXIs',# if testing else '81ASvQUU6xYJzGei9r1HwkIcR3xZgFHrkMBlHtl5FifykxbbodQNPRixEQ5RwN4K5MSJIdqn4xXLWxqKDzfQ5kxPzZoWde6OsZ1rmaz8TKMNA8aOMHQ4Raxj8okxo2bs',
             'grant_type': 'authorization_code',
             'code': value}
     x = requests.post(url, data=myobj)
     token = json.loads(x.text)
-    app.logger.info("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa22222")
+    app.logger.info("debug 2")
     #get user info by token
-    
+    app.logger.info(f'Sending request, value:{value}, token:{token}')
     info = json.loads( requests.post("http://stonks.goto.msk.ru/api/bank/",headers={'Authorization':f'Bearer {token}'}) )
+    app.logger.info("debug 3")
     transferring_to_db = {"function": "ncredit", "user_hash": token, "sum": message_from_ui["sum"], "mac_address": message_from_ui["mac"], "user_email":info["email"], "full_name":(info["first_name"]+info["last_name"])}
     channel.basic_publish(exchange='',
                     routing_key='db',
                     body=json.dumps(transferring_to_db))
-    app.logger.info("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa333333")
-
-# @app.route('/api/get_token', methods=['POST'])
-# # def get_t():
+    app.logger.info("debug 4")
 
 
 @app.route('/auth', methods=['GET', 'POST'])
 def request_auth():    
-    return redirect(f'/form=?token={token}')
-
-
-
-# @app.route('/login')
-# def rel_login():
-#     t = 'boo'
-#     return t.text
+    return redirect(f'/form=?code={request.args.get("code")}')
 
 
 if __name__ == "__main__":
